1da109b5b8c11b584aec704fa2d963150fba16a7
Talha Erkul <talhadoga@hotmail.com>
2024-10-15 19:35:07 +0300

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    POSTGRES_URL:
-      "postgresql://postgres:eoW1gfGdv3pTODzo@54.243.12.78:5432/chatgpt_clone?schema=public",
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.alias["@sqlite"] = "./database.db";
+    }
+    return config;
   },
 };
 
